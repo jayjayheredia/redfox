3e6d234dbffdecf55033e5ebfe5e1808f1feffdd
jayjayheredia <jjh_julian@hotmail.com>
2025-07-21 16:20:19 -0300

    Migrado a Vite con Firebase modular

--- src/index.js
+++
@@ -1,7 +0,0 @@
-function scrollToSection(event, id) {
-    event.preventDefault(); // No cambia la URL
-    const section = document.getElementById(id);
-    if (section) {
-        section.scrollIntoView({ behavior: 'smooth' });
-    }
-}
